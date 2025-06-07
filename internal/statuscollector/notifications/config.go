6e5b182d8f52f1286b520016e858074b163b9eff
Uladzimier Kuzniacou <ukuzniacou@chili-publish.com>
2025-06-07 11:52:59 +0200

    chore: remove pushover creds defaults for dev

--- internal/statuscollector/notifications/config.go
+++ internal/statuscollector/notifications/config.go
@@ -2,6 +2,6 @@ package notifications
 
 type PushOverConfig struct {
 	ApiUrl string `env:"NOTIFICATION_PUSHOVER_API_URL" envDefault:"https://api.pushover.net/1/messages.json"`
-	Token  string `env:"NOTIFICATION_PUSHOVER_TOKEN,required"` // aay6otxvgv5zwkwck6r6r6bch4qucs
-	User   string `env:"NOTIFICATION_PUSHOVER_USER,required"`  // uun179bk9o34gn7tg3qk8s4jt8d4i5
+	Token  string `env:"NOTIFICATION_PUSHOVER_TOKEN,required"`
+	User   string `env:"NOTIFICATION_PUSHOVER_USER,required"`
 }
